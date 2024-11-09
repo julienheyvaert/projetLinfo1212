9c12f6066bf4df297f430f88539ec2a1f56441b3
Julien Heyvaert <julienheyvaert@protonmail.com>
2024-11-09 17:50:33 +0100

    basic sign up

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-//APP CONFIG
+// APP CONFIG
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("static"));
-
-// ROUTES CONFIG
-// simple routes
-app.get('/', function(req, res) {
-    res.render('homepage');
-});
-
-app.get('/incident', function(req, res) {
-    res.render('incident');
-  });
-
-app.get('/login', function(req, res) {
-res.render('login');
-});
+app.use(express.urlencoded({ extended: true }));
 
 // DB CONFIG
 const { MongoClient } = require("mongodb");
@@ -31,14 +18,76 @@ const incidents_collection = 'incidents_collection';
 async function run_db_connection() {
   try {
     const database = client.db(db_name);
-    const users_c = database.collection(user_collection);
-    const incidents_c = database.collection(incidents_collection);
+    const users_collection = database.collection(user_collection);
+    const incidents_col = database.collection(incidents_collection);
 
-    console.log('Connected.');
+    async function getIncidents(nb_incidents = 10) {
+      return await incidents_col.find({}).limit(nb_incidents).toArray();
+    }
+
+    // Homepage route
+    app.get("/", async (req, res) => {
+      const incidents_c = await getIncidents();
+      res.render("homepage", { incidents_c });
+    });
+
+    // Incident route
+    app.get("/incident", (req, res) => {
+      res.render("incident");
+    });
+
+    // Login route
+    app.get("/login", (req, res) => {
+      res.render("login");
+    });
+
+    // Signup verification route
+    app.post("/signup_verification", async (req, res) => {
+      // handling form infos 
+      const username_sign_up = req.body.username_sign_up;
+      const password_sign_up = req.body.password_sign_up;
+      const completeName_sign_up = req.body.completeName_sign_up;
+      const email_sign_up = req.body.email_sign_up;
+
+      if (!username_sign_up || !password_sign_up || !completeName_sign_up || !email_sign_up) {
+        console.log("An iput is empty.");
+        return res.render("login", { error: "Fill in all the inputs." });
+      }
+
+      // unique username verif
+      const username_taken = await users_collection.findOne({ username: username_sign_up });
+      if (username_taken) {
+        console.log("username already used.");
+        return res.render("login", { error: "Username already used." });
+      }
+
+      // unique email verif
+      const email_taken = await users_collection.findOne({ email: email_sign_up });
+      if (email_taken) {
+        console.log("Email already used.");
+        return res.render("login", { error: "Email already used." });
+      }
+
+      // insertion
+      const new_user_json = {
+        username: username_sign_up,
+        password: password_sign_up,
+        fullName: completeName_sign_up,
+        email: email_sign_up
+      };
+      
+      await users_collection.insertOne(new_user_json);
+
+      // get incidents for hompgae
+      const incidents_c = await getIncidents();
+      res.render("homepage", { incidents_c });
+    });
+    
     app.listen(8080)
 
-  } finally {
-    await client.close();
+  } catch (error) {
+    console.error("Connection error:", error);
   }
 }
+
 run_db_connection();
